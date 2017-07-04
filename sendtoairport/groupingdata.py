ecdd5cfa0923d0b067ef6ecf81e5007c972b070c
dchen187 <chendonghui6@gmail.com>
2017-07-04 15:20:39 +0800

    direct driver

--- sendtoairport/groupingdata.py
+++ sendtoairport/groupingdata.py
@@ -15,39 +15,39 @@ from sendtoairport import StartSchedule
 
 
 # 将订单信息转换为json数组[{numberoforder:3,OrderInfos:[{orderpoid:gfg,duration:1800},{orderpoid:jhj,duration:1800}]},{....}]
-# def incodejs(timepointVec, scheduleDataVec):
-#     car = []
-#     for i in xrange(len(timepointVec)):
-#         for element in scheduleDataVec[i]:
-#             d = {}
-#             d['numberoforder'] = len(element)
-#             ord = []
-#             for element2 in element:
-#                 f = {}
-#                 f['BID'] = element2
-#                 f['timetable'] = timepointVec[i]
-#                 ord.append(f)
-#             d['OrderInfos'] = ord
-#             car.append(d)
-#     jsondatar = json.dumps(car, ensure_ascii=False, separators=(',', ':')).encode('utf-8')
-#     return jsondatar
-def incodejs(AllscheduleData, scheduleDataVec):
+def incodejs(timepointVec, scheduleDataVec):
     car = []
-    tmpIdx = 1
-    for element4 in scheduleDataVec:
-        for element1 in element4:
-            for element2 in element1:
-                for element3 in AllscheduleData:
-                    if element2 == element3['BID']:
-                        f = {}
-                        f["orderNum"] = element3['BID']
-                        f["lng"] = element3['bdlng']
-                        f["lat"] = element3['bdlat']
-                        f["idx"] = tmpIdx
-                        car.append(f)
-            tmpIdx = tmpIdx + 1
+    for i in xrange(len(timepointVec)):
+        for element in scheduleDataVec[i]:
+            d = {}
+            d['numberoforder'] = len(element)
+            ord = []
+            for element2 in element:
+                f = {}
+                f['BID'] = element2
+                f['timetable'] = timepointVec[i]
+                ord.append(f)
+            d['OrderInfos'] = ord
+            car.append(d)
     jsondatar = json.dumps(car, ensure_ascii=False, separators=(',', ':')).encode('utf-8')
     return jsondatar
+# def incodejs(AllscheduleData, scheduleDataVec):
+#     car = []
+#     tmpIdx = 1
+#     for element4 in scheduleDataVec:
+#         for element1 in element4:
+#             for element2 in element1:
+#                 for element3 in AllscheduleData:
+#                     if element2 == element3['BID']:
+#                         f = {}
+#                         f["orderNum"] = element3['BID']
+#                         f["lng"] = element3['bdlng']
+#                         f["lat"] = element3['bdlat']
+#                         f["idx"] = tmpIdx
+#                         car.append(f)
+#             tmpIdx = tmpIdx + 1
+#     jsondatar = json.dumps(car, ensure_ascii=False, separators=(',', ':')).encode('utf-8')
+#     return jsondatar
 
 
 def geteachTimepointSchedule(AllscheduleData):
@@ -67,8 +67,8 @@ def geteachTimepointSchedule(AllscheduleData):
             else:
                 onlyone = [[timepointorder[0]['BID']]]
                 scheduleDataVec.append(onlyone)
-    jsondata = incodejs(AllscheduleData, scheduleDataVec)
-    # jsondata = incodejs(timepointVec, scheduleDataVec)
+    # jsondata = incodejs(AllscheduleData, scheduleDataVec)
+    jsondata = incodejs(timepointVec, scheduleDataVec)
     return jsondata
 
 
